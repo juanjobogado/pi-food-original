cd90bb207cc639452ed7dd3b4e613c1a5e692a6b
juanjobogado <juanjobogado13@gmail.com>
2022-10-24 22:30:48 -0300

    finished backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const recipes = require("./recipes");
+const type = require("./type")
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -9,6 +10,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use("/", recipes);
+router.use("/", type);
 
 
 module.exports = router;
--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -1,2 +1,39 @@
-const { Router } = require("express");
+const Router = require('express');
+const {Recipe, Type} = require('../db.js');
 const router = Router();
+
+const dietsLocal =  [
+    "Gluten Free",
+    "Ketogenic",
+    "Lacto Ovo Vegetarian",
+    "Vegan",
+    "Pescatarian",
+    "Paleolithic",
+    "Primal",
+    "Fodmap Friendly",
+    "Whole 30",
+    "Dairy free",
+    "Vegetarian",
+    "Lacto vegetarian",
+    "Ovo vegetarian"                        
+];
+
+router.get("/diets", async (req, res) => {
+    try{
+    const diets = dietsLocal.forEach(async (e) => {
+        await Type.findOrCreate({ //Ingresa los datos a la tabla si no existen
+             where: {name: e} //donde el name sea cada una de las dietas del Array Local
+         });
+    });        
+        const allDiets = await Type.findAll();         
+        res.status(200).json(allDiets);
+
+    }catch(e){
+        res.status(400).json({message: e})
+    }
+});
+
+
+
+
+module.exports = router;
